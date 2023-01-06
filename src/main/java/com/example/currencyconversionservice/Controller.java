2ccc92cca8d2c225b944377d4e70b53f347d5ead
AldrinoS <aldrinosc@gmail.com>
2023-01-06 18:57:08 +0530

    Added service call using feign and eureka client

--- src/main/java/com/example/currencyconversionservice/Controller.java
+++ src/main/java/com/example/currencyconversionservice/Controller.java
@@ -1,5 +1,6 @@
 package com.example.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,9 @@ import java.util.HashMap;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    CurrencyExchangeProxy proxy;
 
     public static final String CURRENCY_EXCHANGE_URL = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
 
@@ -26,6 +30,19 @@ public class Controller {
         CurrencyConversion result = currencyConversion.getBody();
         result.setQuantity(quantity);
         result.setTotalCalculatedAmount(quantity.multiply(result.getConversionMultiple()));
+        result.setEnvironment(result.getEnvironment() + " Rest Template");
+
+        return result;
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateConversionFeign(@PathVariable String from,
+                                                  @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion result = proxy.retrieve(from, to);
+        result.setQuantity(quantity);
+        result.setTotalCalculatedAmount(quantity.multiply(result.getConversionMultiple()));
+        result.setEnvironment(result.getEnvironment() + " Feign");
 
         return result;
     }
